74808413a22a9751ad0e5a8f836bbb25afb1f614
Amal k dev <amaldev@expressbase.com>
2019-12-02 18:43:54 +0530

    appversion and DeviceId

--- Helpers/NativeHelper.cs
+++ Helpers/NativeHelper.cs
@@ -11,6 +11,7 @@ using Android.Views;
 using Android.Widget;
 using ExpressBase.Mobile.Droid.Helpers;
 using ExpressBase.Mobile.Models;
+using static Android.Provider.Settings;
 
 [assembly: Xamarin.Forms.Dependency(typeof(NativeHelper))]
 [assembly: Xamarin.Forms.Dependency(typeof(ToastMessage))]
@@ -19,6 +20,44 @@ namespace ExpressBase.Mobile.Droid.Helpers
     public class NativeHelper : INativeHelper
     {
         public NativeHelper() { }
+
+        private string _devoiceid;
+
+        public string DeviceId
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(_devoiceid))
+                    return _devoiceid;
+
+                _devoiceid = Android.OS.Build.Serial;
+                if (string.IsNullOrWhiteSpace(_devoiceid) || _devoiceid == Build.Unknown || _devoiceid == "0")
+                {
+                    try
+                    {
+                        var context = Android.App.Application.Context;
+                        _devoiceid = Secure.GetString(context.ContentResolver, Secure.AndroidId);
+                    }
+                    catch (Exception ex)
+                    {
+                        Android.Util.Log.Warn("DeviceInfo", "Unable to get id: " + ex.ToString());
+                    }
+                }
+
+                return _devoiceid;
+            }
+        }
+
+        public string AppVersion
+        {
+            get
+            {
+                var context = Android.App.Application.Context;
+                var info = context.PackageManager.GetPackageInfo(context.PackageName, 0);
+
+                return $"{info.VersionName}.{info.VersionCode.ToString()}";
+            }
+        }
 
         public void CloseApp()
         {
