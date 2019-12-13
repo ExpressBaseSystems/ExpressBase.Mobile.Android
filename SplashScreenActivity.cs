4185abad7455fda6eeee6e35e822078ba035cc60
Amal k dev <amaldev@expressbase.com>
2019-12-13 20:23:12 +0530

    splash activity

---
+++ SplashScreenActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ExpressBase.Mobile.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash",
+              MainLauncher = true,
+              NoHistory = true, Icon = "@mipmap/icon")]
+    public class SplashScreenActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)((int)Window.DecorView.SystemUiVisibility ^ (int)SystemUiFlags.LayoutStable ^ (int)SystemUiFlags.LayoutFullscreen);
+            Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+            Window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
+            base.OnCreate(bundle);
+            this.StartActivity(typeof(MainActivity));
+        }
+    }
+}
