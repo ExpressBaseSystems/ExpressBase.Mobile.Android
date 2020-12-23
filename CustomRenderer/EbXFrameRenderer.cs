7b96f963831fe90bfeabd033c2aa3f794eab44af
Amal K Dev <amaldev@expressbase.com>
2020-12-23 16:50:30 +0530

    renderer

---
+++ CustomRenderer/EbXFrameRenderer.cs
@@ -0,0 +1,35 @@
+﻿using Android.Content;
+using ExpressBase.Mobile.CustomControls;
+using ExpressBase.Mobile.Droid.CustomRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using FrameRenderer = Xamarin.Forms.Platform.Android.AppCompat.FrameRenderer;
+
+[assembly: ExportRenderer(typeof(EbXFrame), typeof(EbXFrameRenderer))]
+
+namespace ExpressBase.Mobile.Droid.CustomRenderer
+{
+    public class EbXFrameRenderer : FrameRenderer
+    {
+        public EbXFrameRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+
+            EbXFrame element = e.NewElement as EbXFrame;
+
+            //if (element == null) return;
+
+            //if (element.HasShadow)
+            //{
+            //    Elevation = 30.0f;
+            //    TranslationZ = 0.0f;
+            //    SetZ(30f);
+            //}
+        }
+    }
+}
